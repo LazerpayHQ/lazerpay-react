a8eedc578dec63f92c8af0a8cf50d0400ecc460a
Njoku Emmanuel <kalunjoku123@gmail.com>
2022-01-25 23:56:19 +0100

    Updates

---
+++ precache-manifest.9be9cff43cae2befff2aa9ca47d02c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf966919e56a33a89ee3d31668999fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59faf00528231577843c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "28a02469540b337a26d0",
+    "url": "./static/js/2.7e764284.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7e764284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59faf00528231577843c",
+    "url": "./static/js/main.d570f1cd.chunk.js"
+  },
+  {
+    "revision": "f83b6c7638d71b6c3fcd",
+    "url": "./static/js/runtime-main.6aa5f620.js"
+  }
+]);
